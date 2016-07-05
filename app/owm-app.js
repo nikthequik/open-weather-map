ed9e5913bc42c2093ebbca827b5a0c7dcc24abae
nikthequik <quik.the.nick@gmail.com>
2016-07-05 00:54:45 -0600

    -add city and home partials with routes

--- app/owm-app.js
+++ app/owm-app.js
@@ -1,3 +1,16 @@
-var app = angular.module('OWMApp', ['ngRoute']);
-
-
+angular.module('OWMApp', ['ngRoute'])
+    .config(['$routeProvider', function($routeProvider) {
+        $routeProvider.when('/', {
+            templateUrl : 'home.html',
+            controller : 'HomeCtrl'
+        }).when('/city', {
+            templateUrl : 'city.html',
+            controller : 'CityCtrl'
+        });
+    }])
+    .controller('HomeCtrl', function($scope) {
+        //empty for now
+    })
+    .controller('CityCtrl', function($scope) {
+        $scope.city = 'New York';
+    });
