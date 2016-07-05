34126a764df712b249ab7c219f4eae24d11cdde6
nikthequik <quik.the.nick@gmail.com>
2016-07-05 13:16:49 -0600

    -add routes and resolve

--- app/owm-app.js
+++ app/owm-app.js
@@ -3,14 +3,32 @@ angular.module('OWMApp', ['ngRoute'])
         $routeProvider.when('/', {
             templateUrl : 'home.html',
             controller : 'HomeCtrl'
-        }).when('/city', {
+        }).when('/cities/:city', {
             templateUrl : 'city.html',
-            controller : 'CityCtrl'
-        });
+            controller : 'CityCtrl',
+            resolve : {
+            	city : function(owmCities, $route, $location) {
+            		var city = $route.current.params.city;
+            		if(owmCities.indexOf(city) === -1) {
+			        $location.path('/error');
+			        return;
+	    			}
+	    			return city;
+            	}
+            }
+        }).when('/error', {
+        	template: '<p> Error - Page Not Found </p>'
+        }).otherwise('/error');
     }])
+    .run(['$rootScope', '$location', function($rootScope, $location) {
+			$rootScope.$on('$routeChangeError', function() {
+				$location.path('/error');
+			});
+	}])
     .controller('HomeCtrl', function($scope) {
         //empty for now
     })
-    .controller('CityCtrl', function($scope) {
-        $scope.city = 'New York';
-    });
+	.controller('CityCtrl', function($scope, city) {
+	    $scope.city = city;
+	})
+    .value('owmCities', ['New York', 'Dallas', 'Chicago']);
